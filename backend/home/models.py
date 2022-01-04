b44f7ca9db8754378e686771b8ddc7bccba05505
Crowdbotics <team@crowdbotics.com>
2022-01-04 15:22:06 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
 from django.db import models
+
+
+class Address(models.Model):
+    "Generated Model"
+    city = models.CharField(
+        max_length=256,
+    )
+    country = models.CharField(
+        max_length=256,
+    )
